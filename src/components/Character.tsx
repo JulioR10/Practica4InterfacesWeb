cad7496e1223888f5d98dc68249cc428d2e5669c
Julio <rodriguez.diez.julio@gmail.com>
2023-04-28 11:12:35 +0200

    Second commit

--- src/components/Character.tsx
+++ src/components/Character.tsx
@@ -1,22 +1,22 @@
-import { useQuery, gql } from "@apollo/client";
+// import { useQuery, gql } from "@apollo/client";
 
-const CHARACTER_QUERY = gql`
-  query Character($id: ID!) {
-    character(id: $id) {
-      name
-    }
-  }
-`;
+// const CHARACTER_QUERY = gql`
+//   query Character($id: ID!) {
+//     character(id: $id) {
+//       name
+//     }
+//   }
+// `;
 
-function Character({ id }: { id: any }) {
-  const { loading, error, data } = useQuery(CHARACTER_QUERY, {
-    variables: { id },
-  });
+// function Character({ id }: { id: any }) {
+//   const { loading, error, data } = useQuery(CHARACTER_QUERY, {
+//     variables: { id },
+//   });
 
-  if (loading) return <p>Loading...</p>;
-  if (error) return <p>Error: {error.message}</p>;
+//   if (loading) return <p>Loading...</p>;
+//   if (error) return <p>Error: {error.message}</p>;
 
-  return <h1>{data.character.name}</h1>;
-}
+//   return <h1>{data.character.name}</h1>;
+// }
 
-export default Character;
+// export default Character;
